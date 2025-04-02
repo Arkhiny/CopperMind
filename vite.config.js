cdb6938135254569db67e4857dea35aaffb02602
Arkhiny <your_email@example.com>
2025-04-02 17:28:49 -0300

    added config to vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000, // Specify the port here
+    historyApiFallback: true, // This ensures that React Router handles routing on all paths
   },
-
 })
